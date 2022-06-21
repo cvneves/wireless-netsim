5678275c3ac8a9c53ea011cc3fa6303ce1437358
Carlos <carloskic1@gmail.com>
2022-06-21 00:10:07 -0300

    graficos torados

--- generate_graphs.sh
+++ generate_graphs.sh
@@ -4,7 +4,6 @@ for file in output/*.txt; do
 	python3 plot_state.py $1 $file graph/$(basename $file .txt).pdf
 	# python3 plot_state.py instances/a.txt $file graph/$(basename $file .txt).pdf
 	# echo output/$(basename $file .txt).png
+	rm -rf graph/out.pdf
+	pdfunite graph/* graph/out.pdf
 done
-
-rm -rf graph/out.pdf
-pdfunite graph/* graph/out.pdf
